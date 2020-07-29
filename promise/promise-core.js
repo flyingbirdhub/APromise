e65e0c461b1fe8ba49cdbfb98c79dbce4a68a741
chinazhonghao <1825547767@qq.com>
2020-07-29 09:44:15 +0800

    resolve function with some problem

--- promise/promise-core.js
+++ promise/promise-core.js
@@ -1,4 +1,4 @@
-const {Once, NextTick, IsFunction, IsObject} = require("./promise-util.js");
+const { Once, NextTick, IsFunction, IsObject } = require("./promise-util.js");
 
 const PROMISE_STATE = {
     pending: "pending",
@@ -6,62 +6,62 @@ const PROMISE_STATE = {
     rejected: "rejected"
 };
 
-function _Promise(resolver){
-    if(!(this instanceof _Promise)){
+function _Promise(resolver) {
+    if (!(this instanceof _Promise)) {
         throw "SelfPromise must be called by new!"
     }
 
     // 回调队列
     this.fulfilledCB = [],
-    this.rejectedCB = [],
-    this.state = PROMISE_STATE.pending;
+        this.rejectedCB = [],
+        this.state = PROMISE_STATE.pending;
     this.fulfilledValue = undefined, this.rejectedValue = undefined;
-    
-    let reject = Once(_reject.bind(this));
-    let resolve = Once(_resolve.bind(this));
 
-    if(typeof resolver === "function"){
+    let reject = _reject.bind(this);
+    let resolve = _resolve.bind(this);
+
+    if (typeof resolver === "function") {
         try {
             resolver(resolve, reject);
         }
-        catch(e){
-            reject(e); 
+        catch (e) {
+            reject(e);
         }
     }
 }
 
-function resolvePromise(promise, x, resolve, reject){
-    if(promise === x){
+function resolvePromise(promise, x, resolve, reject) {
+    if (promise === x) {
         reject(TypeError("promise and x refer to the same object"))
     }
-    
-    if(x instanceof _Promise){
+
+    if (x instanceof _Promise) {
         x.then(resolve, reject);
     }
     else {
-        if(IsObject(x) || IsFunction(x)){
+        if (IsObject(x) || IsFunction(x)) {
             try {
                 let then = x.then;
                 let once = false;
-                if(IsFunction(then)){
+                if (IsFunction(then)) {
                     try {
-                        then.call(x, function(y){
-                            if(once){
+                        then.call(x, function (y) {
+                            if (once) {
                                 return;
                             }
                             once = true;
-                            resolvePromise(promise, y, resolve);
-                        }, function(y){
-                            if(once){
+                            resolvePromise(promise, y, resolve, reject);
+                        }, function (y) {
+                            if (once) {
                                 return;
                             }
                             once = true;
                             reject(y);
                         });
                     }
-                    catch(e){
-                        if(once){
-                            throw e;
+                    catch (e) {
+                        if (once) {
+                            return;
                         }
                         else {
                             reject(e);
@@ -72,7 +72,7 @@ function resolvePromise(promise, x, resolve, reject){
                     resolve(x);
                 }
             }
-            catch(e){
+            catch (e) {
                 reject(e);
             }
         }
@@ -82,47 +82,65 @@ function resolvePromise(promise, x, resolve, reject){
     }
 }
 
-function _reject(reason){
-    if(this.state !== PROMISE_STATE.pending){
+function _reject(reason) {
+    if (this.state !== PROMISE_STATE.pending) {
         return;
     }
     this.state = PROMISE_STATE.rejected;
     this.rejectedValue = reason;
     let cbs = this.rejectedCB.slice(0);
-    for(let cb of cbs){
+    for (let cb of cbs) {
         cb(this.rejectedValue);
     }
     return this;
 }
 
+function NeedResolve(value){
+    if(value instanceof _Promise){
+        return true;
+    }
+
+    if(IsFunction(value) || IsObject(value)){
+        if(IsFunction(value.then)){
+            return true;
+        }
+    }
+    return false;
+}
+
 function _resolve(value) {
-    if(this.state !== PROMISE_STATE.pending){
+    if(NeedResolve(value)){
+        resolvePromise(this, value, this.resolve.bind(this), this.reject.bind(this));
+        return this;
+    }
+    
+    if (this.state !== PROMISE_STATE.pending) {
         return;
     }
     this.state = PROMISE_STATE.fulfilled;
     this.fulfilledValue = value;
     let cbs = this.fulfilledCB.slice(0);
-    for(let cb of cbs){
+    for (let cb of cbs) {
         cb(this.fulfilledValue);
     }
     return this;
 }
 
-_Promise.resolved = function(value){
+_Promise.resolved = function (value) {
     let promise = new _Promise();
     return promise.resolve(value);
 }
 
-_Promise.rejected = function(reason){
+_Promise.rejected = function (reason) {
     let promise = new _Promise();
     return promise.reject(reason);
 }
 
-_Promise.prototype.reject = function (reason){
+_Promise.prototype.reject = function (reason) {
     return _reject.call(this, reason);
 }
 
-_Promise.prototype.resolve = function (value){
+_Promise.prototype.resolve = function (value) {
     return _resolve.call(this, value);
 }
 
@@ -131,46 +149,66 @@ _Promise.prototype.then = function (onFulfilled, onRejected) {
     !IsFunction(onRejected) && (onRejected = undefined);
 
     let parent = this;
-    let promise = new _Promise(function(resolve, reject){
-        if(parent.state === PROMISE_STATE.pending){
-            onFulfilled && parent.fulfilledCB.push(NextTick(function(){
+    let promise = new _Promise(function (resolve, reject) {
+        if (parent.state === PROMISE_STATE.pending) {
+            parent.fulfilledCB.push(NextTick(function () {
                 try {
-                    let x = onFulfilled(parent.fulfilledValue);
-                    resolvePromise(promise, x, resolve, reject);
+                    if (onFulfilled) {
+                        let x = onFulfilled(parent.fulfilledValue);
+                        resolvePromise(promise, x, resolve, reject);
+                    }
+                    else {
+                        resolve(parent.fulfilledValue);
+                    }
                 }
-                catch(e){
+                catch (e) {
                     reject(e);
                 }
             }));
-            onRejected && parent.rejectedCB.push(NextTick(function(){
+            parent.rejectedCB.push(NextTick(function () {
                 try {
-                    let x = onRejected(parent.rejectedValue);
-                    resolvePromise(promise, x, resolve, reject);
+                    if (onRejected) {
+                        let x = onRejected(parent.rejectedValue);
+                        resolvePromise(promise, x, resolve, reject);
+                    }
+                    else {
+                        reject(parent.rejectedValue);
+                    }
                 }
-                catch(e){
+                catch (e) {
                     reject(e);
                 }
             }));
         }
-        else if(parent.state === PROMISE_STATE.fulfilled){
-            if(!onFulfilled){
+        else if (parent.state === PROMISE_STATE.fulfilled) {
+            if (!onFulfilled) {
                 resolve(parent.fulfilledValue);
             }
             else {
-                NextTick(function(){
-                    let x = onFulfilled(parent.fulfilledValue);
-                    resolvePromise(promise, x, resolve, reject);
+                NextTick(function () {
+                    try {
+                        let x = onFulfilled(parent.fulfilledValue);
+                        resolvePromise(promise, x, resolve, reject);
+                    }
+                    catch (e) {
+                        reject(e);
+                    }
                 })();
             }
         }
-        else if(parent.state === PROMISE_STATE.rejected){
-            if(!onRejected){
+        else if (parent.state === PROMISE_STATE.rejected) {
+            if (!onRejected) {
                 reject(parent.rejectedValue);
             }
             else {
-                NextTick(function(){
-                    let x = onRejected(parent.rejectedValue);
-                    resolvePromise(promise, x, resolve, reject);
+                NextTick(function () {
+                    try {
+                        let x = onRejected(parent.rejectedValue);
+                        resolvePromise(promise, x, resolve, reject);
+                    }
+                    catch (e) {
+                        reject(e);
+                    }
                 })();
             }
         }
@@ -178,7 +216,7 @@ _Promise.prototype.then = function (onFulfilled, onRejected) {
     return promise;
 }
 
-_Promise.prototype.catch = function (onRejected){
+_Promise.prototype.catch = function (onRejected) {
     return this.then(undefined, onRejected);
 }
 
--- test/test.js
+++ test/test.js
@@ -1,6 +1,36 @@
-let {deferred, rejected} = require("../promise/promise-interface.js");
+let { deferred, rejected, resolved } = require("../promise/promise-interface.js");
+var dummy = { dummy: "dummy" }; // we fulfill or reject with this when we don't intend to test against it
+var sentinel = { sentinel: "sentinel" }; // a sentinel fulfillment value to test for with strict equality
+var other = { other: "other" }; // a value we don't want to be strict equal to
 
-rejected({dummy: "dummy"}).then(undefined, function(){
-    console.log("hello");
+function outer (value) {
+    return resolved(value);
+}
+
+function inner (value) {
+    return {
+        then: function (onFulfilled) {
+            onFulfilled(value);
+        }
+    };
+}
+
+function yFactory() {
+    return outer(inner(sentinel));
+}
+
+function xFactory() {
+    return {
+        then: function (resolvePromise) {
+            resolvePromise(yFactory());
+        }
+    };
+}
+
+var promise = resolved(dummy).then(function onBasePromiseFulfilled() {
+    return xFactory();
 });
 
+promise.then(function(value){
+    console.log(value);
+})
